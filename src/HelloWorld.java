5499390e025a44c457e2676e53d8b3770ee7a03e
David Lara <david.a.lara05@gmail.com>
2022-10-11 10:12:26 -0500

    created java

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+// all java code live inside class
+public class HelloWorld {
+    // type "main and then press tab" it will give you the following below on line 4
+    public static void main(String[] args) {
+        // type "sout and then press tab"
+        System.out.println("Hello World!");
+
+    }
+}
