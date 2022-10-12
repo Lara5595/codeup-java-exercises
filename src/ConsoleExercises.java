b132afe6b8bd1ada9d1b32154061019bfa2eda3d
David Lara <david.a.lara05@gmail.com>
2022-10-12 09:08:37 -0500

    created lecture

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,10 @@
+public class ConsoleExercises {
+    public static void main(String[] args) {
+        // This allows us to define a string with placeholders and then pass values for those placeholders
+        String name = "codeup";
+        System.out.printf("Hello there, %s. Nice to see you.%n", name);
+
+
+
+    }
+}
