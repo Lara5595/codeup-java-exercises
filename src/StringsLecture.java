bff0e5f851fa3381d79337a6e7af67651571aa5a
David Lara <david.a.lara05@gmail.com>
2022-10-13 15:16:43 -0500

    created file

---
+++ src/StringsLecture.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class StringsLecture {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String teacher = "teacher";
+        teacher = "not teacher";
+        System.out.println(teacher);
+    }
+}
