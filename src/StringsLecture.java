9735e2ff645c3c0755c8a71511849e12025b98d9
David Lara <david.a.lara05@gmail.com>
2022-10-13 15:34:47 -0500

    added notes

--- src/StringsLecture.java
+++ src/StringsLecture.java
@@ -1,11 +1,25 @@
+
+
 import java.util.Scanner;
 
 public class StringsLecture {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
         String teacher = "teacher";
-        teacher = "not teacher";
         System.out.println(teacher);
+        teacher = "not teacher";
+        String javier = "Javier";
+        String anotherJavier = "Javier";
+
+
+//        javier = teacher + javier;
+        System.out.println(teacher);
+        System.out.println(javier);
+        System.out.println(javier.hashCode());
+        System.out.println(anotherJavier.hashCode());
+        System.out.println(javier == anotherJavier);
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.next();
+
+        System.out.println(javier.equals(userInput));
     }
 }
