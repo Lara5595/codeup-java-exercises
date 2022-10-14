77ad8335b6f7e8f98cd2d5fe849439b10f8d3c8a
David Lara <david.a.lara05@gmail.com>
2022-10-14 09:38:16 -0500

    created file

---
+++ src/StringExercise.java
@@ -0,0 +1,15 @@
+
+    public class StringExercise {
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+
+
+
+    } // end of main
+}   // end of class
--- src/StringsLecture.java
+++ src/StringsLecture.java
@@ -4,22 +4,50 @@ import java.util.Scanner;
 
 public class StringsLecture {
     public static void main(String[] args) {
-        String teacher = "teacher";
-        System.out.println(teacher);
-        teacher = "not teacher";
-        String javier = "Javier";
-        String anotherJavier = "Javier";
-
-
-//        javier = teacher + javier;
-        System.out.println(teacher);
-        System.out.println(javier);
-        System.out.println(javier.hashCode());
-        System.out.println(anotherJavier.hashCode());
-        System.out.println(javier == anotherJavier);
         Scanner scanner = new Scanner(System.in);
-        String userInput = scanner.next();
 
-        System.out.println(javier.equals(userInput));
+        // Creating a String
+        String javier = "Javier";
+
+        // converts the object into a number
+        System.out.println(Integer.toHexString((System.identityHashCode(javier))));
+
+        // Creating another string but with the same  Javier
+        String alsoJavier = "Javier";
+
+        // despite having diffrent variables they have the same hex code which is javier and alsoJavier
+        System.out.println(Integer.toHexString((System.identityHashCode(alsoJavier))));
+
+
+        // changing a string
+        javier = javier + " Ruedas";
+        alsoJavier = javier;
+
+
+        System.out.println(alsoJavier);
+        System.out.println(javier);
+
+        // The hex codes changes because we change their variables.
+        System.out.println(Integer.toHexString((System.identityHashCode(javier))));
+        System.out.println(Integer.toHexString((System.identityHashCode(alsoJavier))));
+
+        // This has the same hex code as the first ones since it has the same value. It pointed to the same memory address.  String immutability
+        String letsTryThis = "Javier";
+        System.out.println(Integer.toHexString((System.identityHashCode(letsTryThis))));
+
+
+        // Using .equals
+
+        String theOriginalJavier = "Javier";
+
+        System.out.println(letsTryThis == theOriginalJavier);
+
+        System.out.println("What's your name?");
+        String userName = scanner.next();
+        // this will come out false since we are not using .equals
+        System.out.println(theOriginalJavier == userName);
+        //  .equals you use it when you compare the strings
+        // there is .equals and .equals ignore which just ignore the lowercase
+        System.out.println(theOriginalJavier.equalsIgnoreCase(userName));
     }
 }
