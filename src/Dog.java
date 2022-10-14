af1809c63756a26bd1f0fc1a491716dbb8532d0f
David Lara <david.a.lara05@gmail.com>
2022-10-14 12:27:58 -0500

    created dog file

---
+++ src/Dog.java
@@ -0,0 +1,15 @@
+public class Dog {
+
+    public static void bark() {
+        System.out.println("Woof woof!");
+    }
+
+    public static void main(String[] args) {
+        bark();
+
+
+
+
+
+    }
+}
