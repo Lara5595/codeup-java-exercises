dfc620634c38fbccbb3aa611faa67b6699c55461
David Lara <david.a.lara05@gmail.com>
2022-10-14 10:42:39 -0500

    created bob file

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -2,9 +2,21 @@
     public class StringExercise {
     public static void main(String[] args) {
 
+    // 1. For each of the following output examples: create a String variable that contains the desired output and print it out to the console, you can do this with only one String variable and one print statement for each output example.
 
+        String message = "We don't need no ";
+        System.out.println(message + "education");
+        message = message + "thought control";
+        System.out.println(message);
 
+        String message2 = ("Check" + " \"this\"" + " out!," + "\"s inside of \"" + "s!");
+        System.out.println(message2);
 
+        String message3 = ("In windows, the main drive is usually C:\\");
+        System.out.println(message3);
+
+        String message4 = ("I can do backslashes \\, double backslashes \\\\%n, and the amazing triple backslash \\\\\\!");
+        System.out.printf(message4);
 
 
 
