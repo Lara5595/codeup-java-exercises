e2e4c14c808d3b65d90a38b7f4fe33b6736ddc17
David Lara <david.a.lara05@gmail.com>
2022-10-14 14:12:37 -0500

    finish method notes

--- src/Dog.java
+++ src/Dog.java
@@ -1,15 +1,48 @@
 public class Dog {
 
-    public static void bark() {
-        System.out.println("Woof woof!");
+    public static String bark() {
+//        System.out.println("Woof woof!");
+        return "Woof woof!";
     }
+
+    // You cannot return  ^^ with a void so when change it to String
+
+    //  Method Overloading
+    public static void bark(int weight) {
+    if (weight < 25){
+        System.out.println("Yip yip");
+    } else {
+        System.out.println("Woof woof");
+    }
+    }
+
+//     public static void bark(int weight, String name) {
+//         System.out.printf("%s", name);
+//     }
+
+
+    // you can have more than 1 bark
+    public static  void bark(int weight, String name) {
+        String output = name + " goes ";
+        if (weight < 25) {
+            output += "yip yip";
+        } else {
+            output += "woof woof";
+        }
+        System.out.println(output);
+    }
+
 
     public static void main(String[] args) {
-        bark();
-
-
-
-
-
+        System.out.println(bark()); // This is because of the return
+//        bark();
+//        bark(15);
+//        bark(50);
+//        bark(50,"Rudo");
+//        bark(15, "Fido");
     }
+
+
+
+
 }
