49783e9c219bf4d3c697da3ff6281e69078bdc4e
David Lara <david.a.lara05@gmail.com>
2022-10-14 11:34:42 -0500

    finished

--- src/Bob.java
+++ src/Bob.java
@@ -13,17 +13,35 @@ public class Bob {
         //Write the Java code necessary so that a user of your command line application can have a conversation with Bob.
 
 
-        String goAgain = "y";
-        while (goAgain.equalsIgnoreCase("y")) {
-            System.out.println("Bob, are you okay?");
-            String userAnswer = scanner.next();
+        String goAgain = "y";  // Created this so when the user answer yes it continues
+        while (goAgain.equalsIgnoreCase("y")) {  // while the user enter yes go again
+            System.out.println("Hello I am B0B the Human");       // Bob introduction
+            String userAnswer = scanner.nextLine();
             if (userAnswer.endsWith("?")) {
-                System.out.println("Who, chill out!");
+                System.out.println("Sure");
+            } else if (userAnswer.endsWith("!")) {
+                System.out.println("Whoa chill out");
+            } else if (userAnswer.equals("")) {
+                System.out.println("Fine. Be That Way!");
+            } else if (userAnswer.equalsIgnoreCase("are robots taking over")) {
+                System.out.println("Of course");
+            } else if (userAnswer.equalsIgnoreCase("tell a joke")) {
+                System.out.printf("Why did the chicken cross the road%n.... idk %n");
+            } else {
+                System.out.println("WhatEver");
             }
+
             System.out.println("Do you want to continue?");
             goAgain = scanner.next();
             scanner.nextLine();
-        }
+        } // end of while loop
+
+
+// Mason method
+//        String userInput, conversation = "";
+//        userInput = scanner.nextLine();
+//
+//        System.out.println();
 
 
 
