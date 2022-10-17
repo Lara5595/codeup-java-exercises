e876a8db670fc2ecd5b3c62885986eddb2e40bba
David Lara <david.a.lara05@gmail.com>
2022-10-16 22:27:59 -0500

    still working on 5 and 6

--- src/HighLow.java
+++ src/HighLow.java
@@ -1,52 +1,28 @@
+import java.sql.SQLOutput;
 import java.util.Random;
 import java.util.Scanner;
 
 public class HighLow {
 
-//    public static int gameDev(int min, int max) {
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.printf("Guess the number from %d to %d", min,max);
-//        int userAnswer = Integer.parseInt(scanner.nextLine());
-//        Random rand = new Random(); // instance of random class
-//
-//
-//    }
-//    public static int getNumber(int min, int max) {
-//        Scanner scanner = new Scanner(System.in);
-//
-//        System.out.printf("Enter a number between %d and %d: ", min, max);
-//        int userAnswer = Integer.parseInt(scanner.nextLine());
-//
-//        if (userAnswer <= max && userAnswer >= min) {
-//            System.out.println("You got it");
-//            return userAnswer;
-//        } else {
-//            System.out.println("Try again");
-//            return getNumber(min, max);
-//        }
-//    }
-
-public static int getNumber(int min, int max) {
+public static int rollDice(int sides){
     Scanner scanner = new Scanner(System.in);
-
-    System.out.printf("Enter a number between %d and %d:", min, max);
+    double rand = Math.random();
+    System.out.printf("Enter the number of sides %d");
     int userAnswer = Integer.parseInt(scanner.nextLine());
-
-    if (userAnswer <= max && userAnswer >= userAnswer) {
-        System.out.println("You got it");
-        return userAnswer;
+    if (userAnswer <= sides) {
+        System.out.println("Random Number: " + rand);
+        return sides;
     } else {
         System.out.println("Try again");
-        return getNumber(min,max);
+        return rollDice(sides);
     }
 }
 
 
+
+
     public static void main(String[] args) {
-//        System.out.println(gameDev(1,100));
-        System.out.println(getNumber(1,10));
-
-
+        System.out.println(rollDice(6));
 
     } // end of main
 }  // end of HighLow
