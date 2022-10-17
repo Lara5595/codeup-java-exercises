e5b96dc34f47163de9e2d68a1b965b1c0c5ec74f
David Lara <david.a.lara05@gmail.com>
2022-10-16 20:44:17 -0500

    working on number 4 and 5 from assignments

--- src/HighLow.java
+++ src/HighLow.java
@@ -1,5 +1,16 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class HighLow {
 
+    public static int gameDev(int min, int max) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Guess the number from %d to %d", min,max);
+        int userAnswer = Integer.parseInt(scanner.nextLine());
+        Random rand = new Random(); // instance of random class
+
+
+    }
     // 5.   public static int factorial(int min, int max) {
 //        Scanner scanner = new Scanner(System.in);
 //        System.out.printf("Enter a integer from  %d to %d: ", min, max);
@@ -19,7 +30,7 @@ public class HighLow {
 //    }
 
     public static void main(String[] args) {
-
+        System.out.println(gameDev(1,100));
 
 
 
