418211d374a940cbcd8e33ecd7a39a94054316fa
David Lara <david.a.lara05@gmail.com>
2022-10-18 12:29:36 -0500

    created inputtest

--- src/util/Input.java
+++ src/util/Input.java
@@ -5,11 +5,22 @@ import java.util.Scanner;
 public class Input {
     private Scanner scanner;
 
+    private String getString;
+    public String getString(){
+        return getString();
+    };
 
 
 
 }
 
 
+
 // 1. Create an input validation class
 //Create a package inside of src named util. Inside of util, create a class named Input that has a private field named scanner. When an instance of this object is created, the scanner field should be set to a new instance of the Scanner class. The class should have the following methods, all of which return command line input from the user:
+//String getString()
+//    boolean yesNo()
+//    int getInt(int min, int max)
+//    int getInt()
+//    double getDouble(double min, double max)
+//    double getDouble()
