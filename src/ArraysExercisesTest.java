7cd2977e6bb3075dc6eed27fd05d004eeab153c2
David Lara <david.a.lara05@gmail.com>
2022-10-19 16:08:03 -0500

    created servernamegenerator

--- src/ArraysExercisesTest.java
+++ src/ArraysExercisesTest.java
@@ -42,12 +42,7 @@ public class ArraysExercisesTest {
         }
         // we know have Javier and Gonzalo in the new array
 
-// 2. Server Name Generator
-        // We are going to build a server name generator. Create a class inside of src named ServerNameGenerator, and follow the specs below.
-        //
-        //Create two arrays whose elements are strings: one with at least 10 adjectives, another with at least 10 nouns.
-        //Create a method that will return a random element from an array of strings.
-        //Add a main method, and inside of your main method select and random noun and adjective and hyphenate the combination and display the generated name to the user
+
 
 
 
---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class ServerNameGenerator {
+    public static void main(String[] args) {
+
+        String [] adjectives = {"goofy", "purring", "gratis", "nutty", "glossy", "youthful", "heady", "heady", "impossible", "pumped"};
+        System.out.println(Arrays.toString(adjectives));
+
+        String [] nouns = {"arm", "month", "roof", "thumb", "branch", "potato", "adjustment", "kitty", "soap", "whip" };
+        System.out.println(Arrays.toString(nouns));
+
+
+
+
+
+
+    } // end of main
+} // end of serverNameGenerator
+
+
+// 2. Server Name Generator
+// We are going to build a server name generator. Create a class inside of src named ServerNameGenerator, and follow the specs below.
+//
+//Create two arrays whose elements are strings: one with at least 10 adjectives, another with at least 10 nouns.
+//Create a method that will return a random element from an array of strings.
+//Add a main method, and inside of your main method select and random noun and adjective and hyphenate the combination and display the generated name to the user
+
