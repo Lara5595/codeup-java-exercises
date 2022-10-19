7428a4a88ef7a165bebeb01273fda40d29ed59f2
David Lara <david.a.lara05@gmail.com>
2022-10-19 15:40:10 -0500

    on number 2

--- src/ArraysExercisesTest.java
+++ src/ArraysExercisesTest.java
@@ -47,7 +47,7 @@ public class ArraysExercisesTest {
         //
         //Create two arrays whose elements are strings: one with at least 10 adjectives, another with at least 10 nouns.
         //Create a method that will return a random element from an array of strings.
-        //Add a main method, and inside of your main method select and random noun and adjective and hyphenate the combination and display the generated name to the user.
+        //Add a main method, and inside of your main method select and random noun and adjective and hyphenate the combination and display the generated name to the user
 
 
 
