c1aa37248a8e49ce4f4589d6a16d42902b8b6b5d
David Lara <david.a.lara05@gmail.com>
2022-11-01 14:11:48 -0500

    added note

--- src/Animals/Animal.java
+++ src/Animals/Animal.java
@@ -39,3 +39,4 @@ public class Animal {
 
 // Create a Quote.Quote class with a RandomQuote method. The method should generate a random number from 1 to 4. Depending on the result, the method should return a different quote. Now edit your animal class from the previous exercise so instead of an appropriate animal noise it makes a random quote. Test your animal class and verify that it now emits a random quote instead of an animal-appropriate noise.
 
+// Comment
--- src/AssesmentPractice/PetDogTest.java
+++ src/AssesmentPractice/PetDogTest.java
@@ -1,6 +1,9 @@
 package AssesmentPractice;
 //        11. Create a PetDogTest class, instantiate a PetDog, and verify that the snuggle method works correctly.
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PetDogTest {
 
     public static void main(String[] args) {
@@ -9,10 +12,28 @@ public class PetDogTest {
 
 
         // #12b
+
         PetDog petDog1 = new PetDog("Daisy", "English Mastiff", true);
         PetDog petDog2 = new PetDog("Rex", "Pit Bull", true);
+        ArrayList<PetDog> petDogs = new ArrayList<>(List.of(petDog, petDog1, petDog2));
 
-//        PetDog.allSnuggle(petDogs);
+        PetDog.allSnuggle(petDogs);
+
+
+
+        // java quiz
+        String[] names = {"Rick", "Stacey", "Brad", "Becca"};
+        String x;
+        try {
+            x = names[100];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Exception caught!");
+            x = "";
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("This will always run.");
+        }
 
 
     }
