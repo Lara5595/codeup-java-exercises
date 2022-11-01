37bdb2f3b434d641a6830026181ee13090b7c716
David Lara <david.a.lara05@gmail.com>
2022-11-01 14:28:21 -0500

    created method

--- src/RecursionLecture.java
+++ src/RecursionLecture.java
@@ -1,4 +1,21 @@
 public class RecursionLecture {
     // Taught by DocRob
 
-}
+
+    // Factorial using recursion
+    public static long fact(long n) {
+        // hallmark #2 (a way to stop)
+        if (n == 1 || n == 2) {
+            return n;
+        }
+
+        // hallmark #1 ( answer is in smaller terms of itself)
+        return n * fact(n - 1);
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(fact(5));
+    }
+
+} // end of recursion
